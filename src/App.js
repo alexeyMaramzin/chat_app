6493073c56d28e97d011397630421dc2b9a74e50
mclilstalk <megabanana@bk.ru>
2023-02-17 15:48:04 +0300

    Test

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./css/index.css";
+import React from 'react';
+import LeftSidebar from "./components/LeftSidebar";
+import NomadList from "./components/NomadList";
+import Chat from "./components/Chat";
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div className="App">
+       <Router>
+           <LeftSidebar/>
+           <NomadList/>
+           <Routes>
+               <Route path="/" element={<Chat/>}/>
+               <Route path="/general" element={<Chat chat='general'/>}/>
+               <Route path="/support" element={<Chat chat='support'/>}/>
+               <Route path="/marketing" element={<Chat chat='marketing'/>}/>
+               <Route path="/thailand" element={<Chat chat='thailand'/>}/>
+               <Route path="/italy" element={<Chat chat='italy'/>}/>
+               <Route path="/italy" element={<Chat chat='italy'/>}/>
+               <Route path="*" element={<div style={{margin: '50px 0 0 100px', fontSize: '30px'}}>Not found</div>}/>
+           </Routes>
+       </Router>
+   </div>
   );
 }
 
